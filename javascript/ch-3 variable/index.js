eb9df4624db90269f83de518c938e3532c77e498
denishadaslani <denishadashlani@gmail.com>
2025-01-30 12:34:29 +0530

    java code

---
+++ javascript/ch-3 variable/index.js
@@ -0,0 +1,12 @@
+let a = 10;
+
+console.log(a);
+
+
+let first;
+let second;
+
+first = parseInt(prompt("Enter first number"));
+second = parseInt(prompt("Enter second number"));
+
+document.write(`sum:`, first + second);
